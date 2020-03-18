98b36a206856281ab91f7a94e19e35b5e2997415
quhengye <quhengye@bwton.com>
2020-03-18 13:19:15 +0800

    初始化cesium配置

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+
+<script>
+import 'cesium/Widgets/widgets.css';
+let Cesium = require('cesium/Cesium')
+ export default {
+   name: 'HelloWorld',
+   mounted () {
+     this.$nextTick(() => {
+       const viewer = new Cesium.Viewer('cesiumContainer')
+       console.log('viewer: ', viewer)
+     })
+   }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
